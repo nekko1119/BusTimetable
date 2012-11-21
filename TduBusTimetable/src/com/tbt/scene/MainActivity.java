89de662ae6371c13806f0e857cc57c5928cc2dea
nekko1119 <taku5050@mail.goo.ne.jp>
2012-11-21 09:42:27 +0900

    make main axtivity

--- TduBusTimetable/src/com/tbt/scene/MainActivity.java
+++ TduBusTimetable/src/com/tbt/scene/MainActivity.java
@@ -1,19 +1,25 @@
-package com.tbt;
+package com.tbt.scene;
+
+import com.tbt.R;
+import com.tbt.R.layout;
+import com.tbt.R.menu;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity
+{
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
